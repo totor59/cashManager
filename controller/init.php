699b05592c465706309e79488c5ef9651acc2d2a
Victor MARECHAL <victormarechal59@gmail.com>
2016-11-30 10:33:01 +0100

    Update init.php

--- controller/init.php
+++ controller/init.php
@@ -4,6 +4,6 @@
    require '../model/'.$classname.'.php';
    }
    spl_autoload_register('chargerClasse');
-   $db = new PDO('mysql:host=localhost;dbname=cashmanager', 'cashmanager', 'cashmanager');
+   $db = new PDO('mysql:host=$$$$$$$$;dbname=$$$$$$$$$$$', '$$$$$$$$$', '$$$$$$$$$$');
    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING); 
    $manager = new CompteManager($db);
